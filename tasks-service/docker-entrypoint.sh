07b0ca30ef2fbf2cc761682865fc18ec8ab98cd2
Arkadiusz Bojarski <arkadiuszbojarski@users.noreply.github.com>
2018-01-04 14:45:50 +0100

    Configured tasks-service to use embedded H2 as default test database.
    Signed-off-by: Arkadiusz Bojarski <arkadiuszbojarski@users.noreply.github.com>

--- tasks-service/docker-entrypoint.sh
+++ tasks-service/docker-entrypoint.sh
@@ -3,4 +3,4 @@ while ! nc -z ${DATABASE_HOST} ${DATABASE_PORT}; do
     echo "Trying to connect to MySQL tasks database at ${DATABASE_HOST}:${DATABASE_PORT}..."
     sleep 10
 done
-java -Djava.security.egd=file:/dev/./urandom -jar /target/app.jar
+java -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=container -jar /target/app.jar
