7a51d3cf33962f254f870093262ef7458ee7aa1a
Arkadiusz Bojarski <arkadiuszbojarski@users.noreply.github.com>
2018-01-04 16:07:52 +0100

    Updated tasks-service entrypoint script to await for mysql database.
    Signed-off-by: Arkadiusz Bojarski <arkadiuszbojarski@users.noreply.github.com>

--- tasks-service/docker-entrypoint.sh
+++ tasks-service/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 while ! nc -z ${DATABASE_HOST} ${DATABASE_PORT}; do
-    echo "Trying to connect to MySQL tasks database at ${DATABASE_HOST}:${DATABASE_PORT}..."
+    echo "Awaiting for MySQL tasks database at ${DATABASE_HOST}:${DATABASE_PORT}..."
     sleep 10
 done
 java -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=container -jar /target/app.jar
