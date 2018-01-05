6b0ec60d34d89206c3ad01571579a077c257098f
Arkadiusz Bojarski <arkadiuszbojarski@users.noreply.github.com>
2018-01-05 06:51:34 +0100

    Added InnoDB engine and utf8 default charset.
    Signed-off-by: Arkadiusz Bojarski <arkadiuszbojarski@users.noreply.github.com>

--- uaa-server/src/main/resources/schema.sql
+++ uaa-server/src/main/resources/schema.sql
@@ -1,12 +1,11 @@
 CREATE TABLE IF NOT EXISTS `accounts` (
-	`username` VARCHAR(500) NOT NULL,
+	`username` VARCHAR(500) NOT NULL UNIQUE,
 	`password` VARCHAR(500) NOT NULL,
 	PRIMARY KEY (`username`)
-);
+) ENGINE=InnoDB	DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `user_roles` (
 	`user_name` VARCHAR(500) NOT NULL,
 	`user_role` VARCHAR(500) NOT NULL,
-	PRIMARY KEY(`user_name`),
 	FOREIGN KEY(`user_name`) REFERENCES accounts(username)
-);
+) ENGINE=InnoDB	DEFAULT CHARSET=utf8;
