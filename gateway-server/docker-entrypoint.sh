441fb621499b25e39f4be1e4fa847f86d6dd3f27
Arkadiusz Bojarski <arkadiuszbojarski@users.noreply.github.com>
2018-01-06 08:41:50 +0100

    Updated services to await for configuration server.
    Signed-off-by: Arkadiusz Bojarski <arkadiuszbojarski@users.noreply.github.com>

--- gateway-server/docker-entrypoint.sh
+++ gateway-server/docker-entrypoint.sh
@@ -3,8 +3,4 @@ while ! echo 'GET /eureka/apps/config HTTP/1.0' | nc -z discovery-server 8761; d
     echo "Awaiting for config server registration..."
     sleep 10
 done
-while ! nc -z ${DATABASE_HOST} ${DATABASE_PORT}; do
-    echo "Awaiting for MySQL tasks database at ${DATABASE_HOST}:${DATABASE_PORT}..."
-    sleep 10
-done
 java -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=container -jar /target/app.jar
--- uaa-server/docker-entrypoint.sh
+++ uaa-server/docker-entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+while ! echo 'GET /eureka/apps/config HTTP/1.0' | nc -z discovery-server 8761; do
+    echo "Awaiting for config server registration..."
+    sleep 10
+done
 while ! nc -z ${DATABASE_HOST} ${DATABASE_PORT}; do
     echo "Awaiting for MySQL tasks database at ${DATABASE_HOST}:${DATABASE_PORT}..."
     sleep 10
